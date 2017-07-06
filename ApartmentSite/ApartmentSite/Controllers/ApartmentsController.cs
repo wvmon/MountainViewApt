4b5af726ea72954825b2c5299f3c697f0e4b2f20
William Montesedoca <wvmon360@gmail.com>
2017-07-05 21:58:52 -0600

    Scaffolding

---
+++ ApartmentSite/ApartmentSite/Controllers/ApartmentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ApartmentSite.Models;
+
+namespace ApartmentSite.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private ApartmentEntities db = new ApartmentEntities();
+
+        // GET: Apartments
+        public ActionResult Index()
+        {
+            return View(db.Apartments.ToList());
+        }
+
+        // GET: Apartments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments.Find(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Apartments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AptId,AptAddress,SqFootage,MonthUtilityFee,MonthParkfee,LastCleanDate,IsVacant")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Apartments.Add(apartment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments.Find(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AptId,AptAddress,SqFootage,MonthUtilityFee,MonthParkfee,LastCleanDate,IsVacant")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(apartment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments.Find(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Apartment apartment = db.Apartments.Find(id);
+            db.Apartments.Remove(apartment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ApartmentSite/ApartmentSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApartmentSite.MvcApplication" Language="C#" %>
